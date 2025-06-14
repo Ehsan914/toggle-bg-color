9daee2fba0a229394ad50fbe3a88d9ecbcc2da3d
Ehsan914 <58367106+Ehsan914@users.noreply.github.com>
2025-06-15 02:15:11 +0600

    refactor: simplify background and text color management in ToggleBGColor component

--- src/components/ToggleBGColor.jsx
+++ src/components/ToggleBGColor.jsx
@@ -4,20 +4,15 @@ import "../style.css"
 
 const ToggleBGColor = () => {
     const [mode, setMode] = useState('Dark');
-    const [textColor, setTextColor] = useState('white');
-    const [backgroundColor, setBackgroundColor] = useState("#232323")
-
     const handleRef = useRef(null);
     
     const handleClick = () => {
-        mode === "Dark" ? (setBackgroundColor("white"), setTextColor("black"),
-         setMode("Light"), handleRef.current.classList.add('move-right'))
-        : (setBackgroundColor("#232323"), setTextColor("white"),
-         setMode("Dark"), handleRef.current.classList.remove('move-right'))
+        mode === "Dark" ? (setMode("Light"), handleRef.current.classList.add('move-right'))
+        : (setMode("Dark"), handleRef.current.classList.remove('move-right'))
     }
 
     return (
-        <div className="container" style={{ backgroundColor, color: textColor }}>
+        <div className={`container ${mode === "Light" ? "light-mode" : "dark-mode"}`}>
             <p>Dark</p>
             <div className="button" id="button" onClick={handleClick}>
                 <div className="handle" ref={handleRef}></div>
